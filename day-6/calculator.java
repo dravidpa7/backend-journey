7661116a9b2f5c9c38b8f71d4d349371e47f091e
dravidpa7 <dravidpa113@gmail.com>
2025-10-13 14:35:08 +0530

    feat(calculator) : Completed

--- day-6/calculator.java
+++ day-6/calculator.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 
 public class calculator extends Frame implements ActionListener {
     TextField display;
@@ -120,6 +123,12 @@ public class calculator extends Frame implements ActionListener {
         setSize(350, 500);
         setTitle("Calculator");
         setBackground(Color.LIGHT_GRAY);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+
         setVisible(true);
     }
 
